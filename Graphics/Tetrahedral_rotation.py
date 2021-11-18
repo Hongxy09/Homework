ed0480a8fe89be51aca1135c1821f4a8c8c60457
Hongxy09 <17853100535@163.com>
2021-11-18 12:20:12 +0800

    去看论文了

--- Graphics/Tetrahedral_rotation.py
+++ Graphics/Tetrahedral_rotation.py
@@ -45,7 +45,6 @@ def get_center_point(point_list, num):
         return None
     data_list = get_data(point_list)
     index = num - 1
-    # 每个面由三个顶点，去掉多的那个，以ABC为例
     data_list = np.delete(data_list, index, axis=1)
     center = (np.array([0, 0, 0]))
     sum = (np.array([0, 0, 0]))
@@ -94,8 +93,8 @@ def get_true_n2(point_list, inner_point, n):
 
 
 def get_vector(num, point_list):
-    # 默认的面顺序为ABC，ADC，ABD，BCD，输出当前面的点列表和数据列表及法向量
-    # 拿个中心点一会算法向量
+    # 默认的面顺序为ABC，ADC，ABD，BCD，输出当前面法向量
+    # 内部点用于计算法向量朝向
     inner_point = get_inner_point(point_list)
     point_list = get_each_panner_point_list(point_list, num)
     vec1 = point_list[1]-point_list[0]
@@ -133,14 +132,11 @@ def get_panner_flag(view_point, point_list):
         v = get_view_vector(view_point, i, point_list)
         n_list.append(n)
         view_vector_list.append(v)
-        print("In panner ", i, "The n is:", n, "The v is:", v)
-    # n_list=[[-18,9,18],[0,-18,0],[18,9,18],[0,0,-36]]# 验证是否是法向量出错
     n_list, view_vector_list = np.array(n_list), np.array(view_vector_list)
     for i in range(4):
         flag = np.dot(n_list[i], view_vector_list[i])
         eye_flag.append(flag)
     eye_flag = np.array(eye_flag)
-    print("panner can see:", eye_flag)
     eye_flag[eye_flag <= 0] = 1  # 小于等于零的面可见，置为1
     eye_flag[eye_flag != 1] = 0  # 没有置为1的是不可见的面，置为0
     print("eye_flag is:", eye_flag)
@@ -216,8 +212,6 @@ def line_can_see(eye_flag, point_list):
         all_line_list.append(line_in_each_paner(
             index, point_list))  # 记录了顶点和边向量
     real_point_list = new_all_line_list(all_line_list)  # 仅记录了顶点信息
-    # 每条边的可见性由面决定，如果面不可见则所有的边都减一
-    # 则在每个不可见的面的边表中遍历所有的边，并用边的正值和相反值检索真边表中是否有相同的，有的话真边表的那个可见性flag-1
     real_point_list_with_eyeflag = []  # 记录顶点和eyeflag
     for i in range(len(real_point_list)):
         real_point_list_with_eyeflag.append([real_point_list[i], 2])
@@ -227,8 +221,7 @@ def line_can_see(eye_flag, point_list):
         if panner_eye_flag == 0:  # eye_flag中为0的是不可见的面，其对应的边要-1
             deduct_one_panner_in_point_list(
                 panner_index, all_line_list, real_point_list_with_eyeflag)
-    print("We can see line is real_point_list_with_eyeflag:",
-          real_point_list_with_eyeflag)
+    # print("We can see line is real_point_list_with_eyeflag:",real_point_list_with_eyeflag)
     return real_point_list_with_eyeflag
 
 
@@ -239,8 +232,7 @@ def draw_point(point_list, inner, view_point, ax):
                zdir='z', c='k', label='point')
     ax.scatter(view_point[0], view_point[1], view_point[2],
                c='r', marker='^', label='view_point')
-    ax.scatter(inner[0], inner[1], inner[2],
-               c='b', label='inner_point')
+    # ax.scatter(inner[0], inner[1], inner[2],c='b', label='inner_point')
     ax.legend(loc='best')
     # 给点标号
     for i in range(len(data_list[0])):
@@ -275,19 +267,20 @@ def draw_line_between_two_point(point_a, point_b, point_list, ax, linecolor='k',
               draw_data_list[2], c=linecolor, linestyle=linestyle)
 
 
-def draw_all_line(point_list, eye_flag, ax):
+def draw_all_line(point_list, eye_flag, ax, linecolor='g', linestyle='--'):
     line_list = line_can_see(eye_flag, point_list)
     see_line_list, notsee_line_list = divide_line(line_list)
     for i in range(len(see_line_list)):
         # print(see_line_list[i][0],"and",see_line_list[i][1])
         draw_line_between_two_point(
-            see_line_list[i][0], see_line_list[i][1], point_list, ax)
+            see_line_list[i][0], see_line_list[i][1], point_list, ax)  # 实线用默认的绘图风格
     for j in range(len(notsee_line_list)):
+        # 虚线用指定的绘图风格
         draw_line_between_two_point(
-            notsee_line_list[j][0], notsee_line_list[j][1], point_list, ax, linecolor='g', linestyle='--')
+            notsee_line_list[j][0], notsee_line_list[j][1], point_list, ax, linecolor, linestyle)
 
 
-def draw_tetrahedral(view_point, inner, point_list):
+def draw_tetrahedral(view_point, inner, point_list, linecolor):
     # 四面体的绘制 view_point=[0,0,0]
     # 画布与坐标轴的绘制
     this_type_str = type(view_point)
@@ -298,12 +291,12 @@ def draw_tetrahedral(view_point, inner, point_list):
     ax.set_xlabel('X Label')
     ax.set_ylabel('Y Label')
     ax.set_zlabel('Z Label')
-    ax.view_init(azim=44, elev=3)
     eye_flag = get_panner_flag(view_point, point_list)
     # 画点
     draw_point(point_list, inner, view_point, ax)
-    # 画边
-    draw_all_line(point_list, eye_flag, ax)
+    # 根据面的可见性画出对应的实现边和虚线边
+    draw_all_line(point_list, eye_flag, ax, linecolor)
+    ax.view_init(azim=49, elev=7)
     plt.show()
 
 
@@ -316,34 +309,32 @@ def check_of_n():
     return n_list
 
 
-def rotate(point_list,theta):
-    if theta>360:
-        theta-=360
-    rotate_matrix=np.zeros((3,3))
-    cos_value=np.cos(theta*np.pi/180)
-    sin_value=np.sin(theta*np.pi/180)
-    rotate_matrix[0][0]=cos_value
-    rotate_matrix[0][1]=-sin_value
-    rotate_matrix[1][0]=sin_value
-    rotate_matrix[1][1]=cos_value
-    rotate_matrix[2][2]=1
-    new_point_list=np.dot(point_list,rotate_matrix)
-    # for i in range(4):
-    #     point=point_list[i]
-    #     new_point_list[i]=np.dot(point,rotate_matrix)
+def rotate(point_list, theta):
+    # 输入旋转角度和原始顶点信息，输出旋转后的顶点信息
+    if theta > 360:
+        theta -= 360
+    rotate_matrix = np.zeros((3, 3))
+    cos_value = np.cos(theta*np.pi/180)
+    sin_value = np.sin(theta*np.pi/180)
+    rotate_matrix[0][0] = cos_value
+    rotate_matrix[0][1] = -sin_value
+    rotate_matrix[1][0] = sin_value
+    rotate_matrix[1][1] = cos_value
+    rotate_matrix[2][2] = 1
+    new_point_list = np.dot(point_list, rotate_matrix)
     return new_point_list
 
 
 def fun():
-    # 初始的
-    theta=30
-    view_point = [9, 3, 1]
+    # 设置旋转角度，虚线颜色，视点坐标
+    theta = 120
+    linecolor = 'r'
+    view_point = [5,5,5]
     point_list = get_point()
     inner = get_inner_point(point_list)
-    draw_tetrahedral(view_point, inner, point_list)
-    # 旋转后的
-    rotated_point_list=rotate(point_list,theta)
+    # 绘制旋转前的图形
+    draw_tetrahedral(view_point, inner, point_list, linecolor)
+    # 绘制旋转后的图形
+    rotated_point_list = rotate(point_list, theta)
     rotated_inner = get_inner_point(rotated_point_list)
-    draw_tetrahedral(view_point, rotated_inner, rotated_point_list)
-
-
+    draw_tetrahedral(view_point, rotated_inner, rotated_point_list, linecolor)
